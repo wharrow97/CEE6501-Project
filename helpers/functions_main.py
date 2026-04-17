b9bb2794acdfb4ee3a36d65b55ed157cdfdbb43e
wharrow97 <wharrow@gatech.edu>
2026-04-17 13:27:07 -0400

    Referencing Folders

---
+++ helpers/functions_main.py
@@ -0,0 +1,14 @@
+# -------------------------
+# Import Libraries and Helper Functions
+# -------------------------
+# This slide imports the required Python libraries and adds the
+# course helper-code directory to the Python path.
+
+import sys
+import os
+import numpy as np
+import pandas as pd
+
+def functions_main(inputs):
+    outputs = inputs + 5
+    return outputs
